c5135e2ef8759312b26fbc9d39935e57f279bc17
Alecaddd <castellani.ale@gmail.com>
2015-04-06 23:51:48 -0700

    Lesson 6: add_theme_support

---
+++ lesson_6/functions.php
@@ -0,0 +1,25 @@
+<?php
+	
+function awesome_script_enqueue() {
+	
+	wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/awesome.css', array(), '1.0.0', 'all');
+	wp_enqueue_script('customjs', get_template_directory_uri() . '/js/awesome.js', array(), '1.0.0', true);
+	
+}
+
+add_action( 'wp_enqueue_scripts', 'awesome_script_enqueue');
+
+function awesome_theme_setup() {
+	
+	add_theme_support('menus');
+	
+	register_nav_menu('primary', 'Primary Header Navigation');
+	register_nav_menu('secondary', 'Footer Navigation');
+	
+}
+
+add_action('init', 'awesome_theme_setup');
+
+add_theme_support('custom-background');
+add_theme_support('custom-header');
+add_theme_support('post-thumbnails');
---
+++ lesson_6/index.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+	<?php 
+	
+	if( have_posts() ):
+		
+		while( have_posts() ): the_post(); ?>
+			
+			<h3><?php the_title(); ?></h3>
+			<div class="thumbnail-img"><?php the_post_thumbnail('large'); ?></div>
+			<small>Posted on: <?php the_time('F j, Y'); ?> at <?php the_time('g:i a'); ?>, in <?php the_category(); ?></small>
+			
+			<p><?php the_content(); ?></p>
+			
+			<hr>
+		
+		<?php endwhile;
+		
+	endif;
+			
+	?>
+
+<?php get_footer(); ?>
