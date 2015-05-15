c2e1938b95ca123ecd0560c2f9e2ae7dbad55472
Alecaddd <castellani.ale@gmail.com>
2015-05-14 19:52:18 -0700

    Lesson 10

---
+++ lesson_10/index.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+
+<div class="row">
+	
+	<div class="col-xs-12 col-sm-8">
+
+		<?php 
+		
+		if( have_posts() ):
+			
+			while( have_posts() ): the_post(); ?>
+				
+				<?php get_template_part('content',get_post_format()); ?>
+			
+			<?php endwhile;
+			
+		endif;
+				
+		?>
+	
+	</div>
+	
+	<div class="col-xs-12 col-sm-4">
+		<?php get_sidebar(); ?>
+	</div>
+	
+</div>
+
+<?php get_footer(); ?>
